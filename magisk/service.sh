b51bd6f3054117315686d88f3b88faf28c37199e
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-19 10:49:37 +0800

    scripts/service: create detach symlink for magisk at every boot

--- magisk/service.sh
+++ magisk/service.sh
@@ -1,3 +1,11 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 [ -f "$MODDIR/detach.txt" ] && "$MODDIR"/system/bin/detach serialize "$MODDIR/detach.txt" "$MODDIR/detach.bin"
+
+# yes we just create the symlink at every boot these tmpfs dirs 
+# magisk will add /debug_ramdisk, /sbin on $PATH
+[ -f /data/adb/magisk/magisk ] && {
+	[ -w /sbin ] && rwdir=/sbin
+	[ -w /debug_ramdisk ] && rwdir=/debug_ramdisk
+	ln -sf $MODDIR/system/bin/detach $rwdir/detach
+}
