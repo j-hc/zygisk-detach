6b8e9260f80b2d4fc71cd75e631c8f27c78f1a71
j-hc <j-hc@users.noreply.github.com>
2024-12-31 14:55:38 +0300

    /system/bin/detach -> /data/adb/modules/zygisk-detach/detach

--- magisk/customize.sh
+++ magisk/customize.sh
@@ -1,11 +1,11 @@
 #!/system/bin/sh
 
-if [ -n "$KSU" ] && [ ! -d "$NVBASE/modules/zygisksu" ]; then
-	abort "You do not have ZygiskNext installed. Bye."
+if [ -n "$KSU" ]; then
+	ui_print "- KernelSU detected. Make sure you are using a Zygisk module!"
 fi
 
-mv -f "$MODPATH/system/bin/detach-${ARCH}" "$MODPATH/system/bin/detach"
-rm "$MODPATH"/system/bin/detach-*
+mv -f "$MODPATH/detach-${ARCH}" "$MODPATH/detach"
+rm "$MODPATH"/detach-*
 
 # preserve detach.bin
 if [ -f "$NVBASE/modules/zygisk-detach/detach.bin" ]; then
@@ -15,11 +15,16 @@ fi
 
 if [ -f "$MODPATH/detach.txt" ]; then
 	ui_print "- detach.txt inside module! Generating detach.bin"
-	OP=$("$MODPATH"/system/bin/detach serialize "$MODPATH/detach.txt" "$MODPATH/detach.bin" 2>&1)
+	OP=$("$MODPATH"/detach serialize "$MODPATH/detach.txt" "$MODPATH/detach.bin" 2>&1)
 	ui_print "$OP"
 fi
 
-ui_print "- Run 'su -c detach' in terminal after the reboot"
+if ! grep -q 'alias detach' /data/data/com.termux/files/home/.bashrc; then
+	echo "alias detach='su -c /data/adb/modules/zygisk-detach/detach'" >>/data/data/com.termux/files/home/.bashrc
+	chmod 777 /data/data/com.termux/files/home/.bashrc
+fi
+
+ui_print "- Run 'detach' in termux after the reboot"
 ui_print "- Or use zygisk-detach-app"
 if [ -n "$KSU" ]; then
 	ui_print "- Or use the WebUI from KernelSU app"
--- magisk/service.sh
+++
@@ -1,3 +0,0 @@
-#!/system/bin/sh
-MODDIR=${0%/*}
-[ -f "$MODDIR/detach.txt" ] && "$MODDIR"/system/bin/detach serialize "$MODDIR/detach.txt" "$MODDIR/detach.bin"
